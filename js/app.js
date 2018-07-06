1fb7fd9c7765fcf188c84a90b093511c687e7391
Alex Khant <grashupfer99@gmail.com>
2018-07-06 18:35:22 +0900

    added post a new tweet,empty field check

--- js/app.js
+++ js/app.js
@@ -4,11 +4,16 @@ const app = express();
 const Twit = require('twit');
 const config = require('./config');
 const moment = require('moment');
+const bodyParser = require("body-parser");
+// To store all user's data in order to add a new tweet later 
+let dataBackup;
 
 // Use Pug.js
 app.set("view engine", "pug");
 // Static files
 app.use(express.static("./"));
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Create a timestamp
 const timeFormatter = (time, type = 0) => {
@@ -28,13 +33,8 @@ const getProfileDataById = async (id) => {
   return myTwit.get("users/lookup", { user_id: id.message_create.sender_id });
 };
 
-// Use Twitter API Client for node
-const myTwit = new Twit({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token: config.access_token,
-  access_token_secret: config.access_token_secret
-});
+// Use Twitter API Client for node, access codes are automatically passed from config.js file.
+const myTwit = new Twit(config);
 
 // Array of promises to request account data for verification, timeline, friends, messages
 const promises = [
@@ -54,34 +54,36 @@ app.get('/', async (req, res, next) => {
 
     // store data in object
     data = {
-        user:data[0].data,
-        timeline: data[1].data,
-        following: data[2].data.users,
-        directMsg: data[3].data.events,
-      };
-      console.log(data.timeline.length);
-      
-      // create timestamps on a user's timeline
-      data.timeline.map( created_at => {
-        created_at.timestamp = timeFormatter(created_at.created_at);
-      });
+      user:data[0].data,
+      timeline: data[1].data,
+      following: data[2].data.users,
+      directMsg: data[3].data.events,
+    };
 
-      // For direct messages, append user's id and timestamp
-      data.directMsg.map(info => {
-        info.message_create.my_id = data.user.id_str;
-        info.formatted_timestamp = timeFormatter(parseInt(info.created_timestamp), 1);
-      });
-
-      // Get user's profile data by id
-      const getData = await Promise.all(data.directMsg.map(info => getProfileDataById(info)));
-        
-      // get image urls for every sender 
-      getData.forEach( (users, i) => {
-        data.directMsg[i].profile_img_url = users.data[0].profile_image_url;
-      });
+    // store all user's data
+    dataBackup = data;
       
-      // render the index page
-      res.render('index', {data});
+    // create timestamps on a user's timeline
+    data.timeline.map( created_at => {
+      created_at.timestamp = timeFormatter(created_at.created_at);
+    });
+
+    // For direct messages, append user's id and timestamp
+    data.directMsg.map(info => {
+      info.message_create.my_id = data.user.id_str;
+      info.formatted_timestamp = timeFormatter(parseInt(info.created_timestamp), 1);
+    });
+
+    // Get user's profile data by id
+    const getData = await Promise.all(data.directMsg.map(info => getProfileDataById(info)));
+      
+    // get image urls for every sender 
+    getData.forEach( (users, i) => {
+      data.directMsg[i].profile_img_url = users.data[0].profile_image_url;
+    });
+    
+    // render the index page
+    res.render('index', {data});
 
     // catch an error and render an error page
   } catch(err){
@@ -90,6 +92,29 @@ app.get('/', async (req, res, next) => {
     next(err);
     return;
   }
+});
+
+// Post a new tweet withouth having to refresh a new page
+app.post('/', (req, res, next)=> {
+
+  // post a new tweet with Twitter API Client
+  myTwit.post('statuses/update', { status: req.body.newTweet }, 
+    (err,dat,response)=> {
+      // Do if no error is found 
+      if(!err){
+        let newTweetObj = dat;
+        newTweetObj.timestamp = timeFormatter(newTweetObj.created_at);
+        dataBackup.timeline.pop(dataBackup.timeline[dataBackup.length-1]);
+        dataBackup.timeline.unshift(newTweetObj);
+        const data = dataBackup;
+        res.render('index', {data});
+      }else {
+        err.details = "Input error detected! Input field should not be empty!";
+        err.status = 500;
+        next(err);
+        return;
+      }
+    });
 });
 
 // Handling errors
@@ -110,5 +135,3 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!");
 });
-
-
